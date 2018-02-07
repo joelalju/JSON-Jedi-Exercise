21c41fc4206f4805444c5644b204c3c7f8ec7322
joelalju <joelalju100@gmail.com>
2018-02-07 17:49:04 +0100

    "Next" button added

--- jeje.js
+++ jeje.js
@@ -1,9 +1,20 @@
+var id = 549; 
+
 $(window).on("load", function() {
-	$.get("https://www.pokeapi.co/api/v2/pokemon/549/", function (data) {
-			$("#id").append(data.id); //ID
-			$("#normal").prepend("<img src="+data.sprites.front_default+" />");
-			$("#shiny").prepend("<img src="+data.sprites.front_shiny+" />");
-			$("#name").append(data.name);
-			$("#type").append(data.types[0].type.name);
-		}, "json");
+	getjson();
+	$("#next").on("click", function() {
+			id++;
+			getjson();
+	})
 });
+
+function getjson() {
+	$.get("https://www.pokeapi.co/api/v2/pokemon/"+id+"/", function (data) {
+			$("#id").text(data.id); //ID
+			$("#normal").html("<img src="+data.sprites.front_default+" />");
+			$("#shiny").html("<img src="+data.sprites.front_shiny+" />");
+			$("#name").text(data.name);
+			$("#type").text(data.types[0].type.name);
+			$("#type2").text(data.types[1].type.name);
+		}, "json");
+}
